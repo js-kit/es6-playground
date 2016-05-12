5421efa60c769acb9915531e8fbe65f581482e96
Joey Zhou <josephyzhou@gmail.com>
2016-05-12 00:25:04 -0400

    more

--- dataStructures.js
+++ dataStructures.js
@@ -18,3 +18,28 @@ function g(...args) {
 function h(a = 1, b = 2) {
 	console.log(a, b);
 }
+h();
+
+// default value as a func
+function i(a = 2, b = a + 3) {
+	console.log(a, b);
+}
+i(5);
+
+// Destructuring array
+function j() {
+	return [1, 2, 3];
+}
+let [j1, j2] = j();
+console.log(a, b);
+
+// Destructuring object
+function k() {
+	return {
+		k1: 1,
+		k2: 2,
+		k3: 3
+	};
+}
+let {k1: k1, k2: k2} = k()
+console.log(k1, k2);
